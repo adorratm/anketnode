a96b9af5acab37a2f47e83d6a00b9aae6b1d9834
Emre KILIÇ <emrekilic19983@gmail.com>
2023-04-12 17:09:58 +0300

    update navbar

--- frontend/rentacarpanel/vendor/js/init.js
+++ frontend/rentacarpanel/vendor/js/init.js
@@ -1,6 +1,6 @@
 /** *************Init JS*********************
 	
-    TABLE OF CONTENTS
+	TABLE OF CONTENTS
 	---------------------------
 	1.Ready function
 	2.Load function
@@ -9,72 +9,75 @@
 	5.Chat App function
 	6.Resize function
  ** ***************************************/
- 
- "use strict"; 
+
+"use strict";
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 /*****Ready function start*****/
-$(document).ready(function(){
+$(document).ready(function () {
 	pinkman();
 	/*Disabled*/
-	$(document).on("click", "a.disabled,a:disabled",function(e) {
-		 return false;
+	$(document).on("click", "a.disabled,a:disabled", function (e) {
+		return false;
 	});
 });
 /*****Ready function end*****/
 
 /*****Load function start*****/
-$(window).on("load",function(){
+$(window).on("load", function () {
 	$(".preloader-it").delay(500).fadeOut("slow");
 });
 /*****Load function* end*****/
 
 /*Variables*/
-var height,width,
-$wrapper = $(".hk-wrapper"),
-$nav = $(".hk-nav"),
-$vertnaltNav = $(".hk-wrapper.hk-vertical-nav,.hk-wrapper.hk-alt-nav"),
-$horizontalNav = $(".hk-wrapper.hk-horizontal-nav"),
-$navbar= $(".hk-navbar");
+var height, width,
+	$wrapper = $(".hk-wrapper"),
+	$nav = $(".hk-nav"),
+	$vertnaltNav = $(".hk-wrapper.hk-vertical-nav,.hk-wrapper.hk-alt-nav"),
+	$horizontalNav = $(".hk-wrapper.hk-horizontal-nav"),
+	$navbar = $(".hk-navbar");
 
 /***** pinkman function start *****/
-var pinkman = function(){
-	
+var pinkman = function () {
+
 	/*Navbar Collapse Animation*/
 	var navbarNavCollapse = $('.hk-nav .navbar-nav  li');
 	var navbarNavAnchor = '.hk-nav .navbar-nav  li a';
-	$(document).on("click",navbarNavAnchor,function (e) {
+	$(document).on("click", navbarNavAnchor, function (e) {
 		if ($(this).attr('aria-expanded') === "false")
-				$(this).blur();
-			$(this).parent().siblings().find('.collapse').collapse('hide');
-			$(this).parent().find('.collapse').collapse('hide');
+			$(this).blur();
+		$(this).parent().siblings().find('.collapse').collapse('hide');
+		$(this).parent().find('.collapse').collapse('hide');
 	});
-	
+
 	/*Card Remove*/
 	$(document).on('click', '.card-close', function (e) {
 		var effect = $(this).data('effect');
-			$(this).closest('.card')[effect]();
-		return false;	
+		$(this).closest('.card')[effect]();
+		return false;
 	});
-	
+
 	/*Accordion js*/
 	$(document).on('show.bs.collapse', '.accordion .collapse', function (e) {
 		$(this).siblings('.card-header').addClass('activestate');
 	});
-	
+
 	$(document).on('hide.bs.collapse', '.accordion .collapse', function (e) {
 		$(this).siblings('.card-header').removeClass('activestate');
 	});
-	
+
 	/*Navbar Toggle*/
 	$(document).on('click', '#navbar_toggle_btn', function (e) {
 		$wrapper.toggleClass('hk-nav-toggle');
-		$(window).trigger( "resize" );
+		$(window).trigger("resize");
 		return false;
 	});
 	$(document).on('click', '#hk_nav_backdrop,#hk_nav_close', function (e) {
 		$wrapper.removeClass('hk-nav-toggle');
 		return false;
 	});
-	
+
 	/*Settings panel Toggle*/
 	$(document).on('click', '#settings_toggle_btn', function (e) {
 		$wrapper.toggleClass('hk-settings-toggle');
@@ -103,55 +106,55 @@ var pinkman = function(){
 		return false;
 	});
 	$(document).on('click', '#navtop_light_select', function (e) {
-		$navbar.removeClass('navbar-dark').addClass('navbar-light').find('img.brand-img').attr('src','vendor/img/logo-light.png');
+		$navbar.removeClass('navbar-dark').addClass('navbar-light').find('img.brand-img').attr('src', 'vendor/img/logo-light.png');
 		return false;
 	});
 	$(document).on('click', '#navtop_dark_select', function (e) {
-		$navbar.removeClass('navbar-light').addClass('navbar-dark').find('img.brand-img').attr('src','vendor/img/logo-dark.png');
+		$navbar.removeClass('navbar-light').addClass('navbar-dark').find('img.brand-img').attr('src', 'vendor/img/logo-dark.png');
 		return false;
 	});
-	
+
 	/*Search form Collapse*/
 	$(document).on('click', '#navbar_search_btn', function (e) {
 		$('html,body').animate({ scrollTop: 0 }, 'slow');
 		$(".navbar-search input").focus();
 		$wrapper.addClass('navbar-search-toggle');
-		$(window).trigger( "resize" );
+		$(window).trigger("resize");
 	});
-	$(document).on('click', '#navbar_search_close',function (e) {
+	$(document).on('click', '#navbar_search_close', function (e) {
 		$wrapper.removeClass('navbar-search-toggle');
-		$(window).trigger( "resize" );
+		$(window).trigger("resize");
 		return false;
 	});
-	
+
 	/*Refresh Init Js*/
 	var refreshMe = '.refresh';
-	$(document).on("click",refreshMe,function (e) {
+	$(document).on("click", refreshMe, function (e) {
 		var panelToRefresh = $(this).closest('.card').find('.refresh-container');
 		var dataToRefresh = $(this).closest('.card').find('.panel-wrapper');
 		var loadingAnim = panelToRefresh.find('.la-anim-1');
 		panelToRefresh.show();
-		setTimeout(function(){
+		setTimeout(function () {
 			loadingAnim.addClass('la-animate');
-		},100);
-		function started(){} //function before timeout
-		setTimeout(function(){
-			function completed(){} //function after timeout
+		}, 100);
+		function started() { } //function before timeout
+		setTimeout(function () {
+			function completed() { } //function after timeout
 			panelToRefresh.fadeOut(800);
-			setTimeout(function(){
+			setTimeout(function () {
 				loadingAnim.removeClass('la-animate');
-			},800);
-		},1500);
-		  return false;
-	});
-	
-	/*Fullscreen Init Js*/
-	$(document).on("click",".full-screen",function (e) {
-		$(this).parents('.card').toggleClass('fullscreen');
-		$(window).trigger( "resize" );
+			}, 800);
+		}, 1500);
 		return false;
 	});
-	
+
+	/*Fullscreen Init Js*/
+	$(document).on("click", ".full-screen", function (e) {
+		$(this).parents('.card').toggleClass('fullscreen');
+		$(window).trigger("resize");
+		return false;
+	});
+
 };
 /***** pinkman function end *****/
 
@@ -161,7 +164,7 @@ var setHeightWidth = function () {
 	width = window.innerWidth;
 	$('.full-height').css('height', (height));
 	$('.hk-pg-wrapper').css('min-height', (height));
-	
+
 };
 /***** Full height function end *****/
 
@@ -169,6 +172,6 @@ var setHeightWidth = function () {
 $(window).on("resize", function () {
 	setHeightWidth();
 });
-$(window).trigger( "resize" );
+$(window).trigger("resize");
 /***** Resize function end *****/
 
